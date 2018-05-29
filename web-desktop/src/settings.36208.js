1ba26eba6f5d01010fd73aa10607cf859652dc88
sy120wxt <sy120wxt@163.com>
2018-05-29 19:10:08 +0800

    Add files via upload

---
+++ web-desktop/src/settings.36208.js
@@ -0,0 +1,148 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "7e/yxVV/1FAbpO5ISBzKxP": [
+                "Texture/doute.jpg",
+                0
+            ],
+            "20gyFdBWpP7qn4+8VgLutG": [
+                "Texture/flag.jpg",
+                0
+            ],
+            "a36f6aj1lP74W+hrjV+GUy": [
+                "Texture/地雷2.jpg",
+                0
+            ],
+            "74Q8OG0J1CUpqzFNpUXIQR": [
+                "Texture/失败开关.jpg",
+                0
+            ],
+            "f0aQGpfwdPManhUnx8nwQu": [
+                "Texture/方块按下.jpg",
+                0
+            ],
+            "bdXcL9Uf9HfqOYGELyHtw9": [
+                "Texture/方块数字1.jpg",
+                0
+            ],
+            "3fbaIUsjJDLLTBNuvc4bzM": [
+                "Texture/方块数字2.jpg",
+                0
+            ],
+            "3afdgP1phC6q7iDdeRStml": [
+                "Texture/方块数字3.jpg",
+                0
+            ],
+            "42QsnFM9dLj6EwBdiSN8Eh": [
+                "Texture/方块数字4.jpg",
+                0
+            ],
+            "6d1bABlkZKhL91O95VO9Bl": [
+                "Texture/方块数字5.jpg",
+                0
+            ],
+            "21lYqeZGJBi51N2/VfYxko": [
+                "Texture/方块数字6.jpg",
+                0
+            ],
+            "48uWkZeRxA55Y5qChHgdrV": [
+                "Texture/方块数字7.jpg",
+                0
+            ],
+            "5bO+v4Fk9ENo1uzFRrL2t3": [
+                "Texture/方块数字8.jpg",
+                0
+            ],
+            "e83O7ga81OW5updkfrAzT2": [
+                "Texture/方块背景3.jpg",
+                0
+            ],
+            "bcEZBT3btGpI0NBibHcE01": [
+                "Texture/方块覆盖图2.jpg",
+                0
+            ],
+            "99Nc96dnxOH5vXtDj+c+ND": [
+                "Texture/重置开关.jpg",
+                0
+            ]
+        },
+        internal: {
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ],
+            "6eBWFz0oVHPLIGQKf/9Thu": [
+                "image/default_sprite.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/Scene/Sweep.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/Sweep.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "057dd198c": [
+            "0fzdFOr5BNBoH0eKKO0wtl",
+            "19aLiAeGJOvrj9mxWs4Dnt",
+            "1fOjbIKBhNu6u3IJAVXars",
+            "30JL/neyZLorgTOye3zQnv",
+            "31f06oHz9Jh7+T9E4CCJej",
+            "4bWkGxe3hF+KsbsHvS56kX",
+            "6danKuY4pO2b5bq/I/DrnT",
+            0,
+            "7fHzkeavBMRYWjCrJr+pnR",
+            "89ohDDUPNOpLucHmciSkLM",
+            "8c20Sso/ZEn7NUfNSM+EBh",
+            "9bvaMerUlDyary99mJa6xp",
+            "9c1VsbPRJIT46VFc6BR2G2",
+            "9e3Aay+zNJSo2cJAGNfnxc",
+            "b1k1MIqXJOeo2VfQWP9jP5",
+            "b335RLeTVE+a789OxJhGbc",
+            "b5xYtj4HVEF7ojqNQpF6DK",
+            "c9CHWQ8OdBWqJWcecixJmR",
+            "d0yiokNdNC97G9Gtco8jDk",
+            "ff0sKl0C5BnYEz9TV8z0UC"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "77+A+rTFZPxYbl3BEBFKTN"
+    ],
+    "md5AssetsMap": {
+        "05/057dd198c.json": "df886",
+        "assets/Texture/doute.jpg": "a779c",
+        "assets/Texture/flag.jpg": "f0cbb",
+        "assets/Texture/地雷2.jpg": "2c9a5",
+        "assets/Texture/失败开关.jpg": "4d7cd",
+        "assets/Texture/方块按下.jpg": "96adc",
+        "assets/Texture/方块数字1.jpg": "c01c1",
+        "assets/Texture/方块数字2.jpg": "94665",
+        "assets/Texture/方块数字3.jpg": "68ede",
+        "assets/Texture/方块数字4.jpg": "4a326",
+        "assets/Texture/方块数字5.jpg": "f858f",
+        "assets/Texture/方块数字6.jpg": "80ad7",
+        "assets/Texture/方块数字7.jpg": "bd0a7",
+        "assets/Texture/方块数字8.jpg": "b6a04",
+        "assets/Texture/方块背景3.jpg": "bb3ae",
+        "assets/Texture/方块覆盖图2.jpg": "686c3",
+        "assets/Texture/重置开关.jpg": "85208",
+        "internal/image/default_panel.png": "cdbc9",
+        "internal/image/default_sprite.png": "68270"
+    }
+};
